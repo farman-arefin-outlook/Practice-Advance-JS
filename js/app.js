7a0a668595e054510edb65540ace284590fd4fbe
Tamim <farman.arefin1@outlook.com>
2021-08-20 20:44:54 +0600

    commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log("This is test section");
+const addButton = document.getElementById('add-task-button');
+const taskList = document.getElementById('task-list');
+
+
+let doneTaskCount = 0;
+
+//function for tracking input value and update
+
+addButton.addEventListener('click', function () {
+    const taskInput = document.getElementById('task-input');
+    const div = document.createElement('div');
+    const para = document.createElement('p');
+    const button = document.createElement('button');
+
+    para.innerText = taskInput.value;
+    button.innerText = 'Done';
+
+
+    button.classList.add('btn', 'btn-primary', 'task-button');
+    div.classList.add()
+});
